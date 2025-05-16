dc7d1a766e7d73ba8619944bfcb6b26a7cb0918c
Gillhad <crbaronv@gmail.com>
2025-05-17 00:01:14 +0200

    [feat]start project

---
+++ app/src/main/java/com/gillhad/color_mind_challenge/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.gillhad.color_mind_challenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.gillhad.color_mind_challenge.ui.theme.Color_mind_challengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Color_mind_challengeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                 Box(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
