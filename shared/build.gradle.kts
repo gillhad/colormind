ce08057fd6f6dde8a28de4ea274e699ddd3ed01a
Gillhad <crbaronv@gmail.com>
2025-06-08 12:50:42 +0200

    [bitrise]add test dependencies in shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -50,6 +50,14 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.main)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
     implementation(libs.bundles.hilt)
     ksp(libs.hilt.compiler)
 
